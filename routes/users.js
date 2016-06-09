b3f92f200121bf7521d04e25b1c4903b99ff19df
Geo Narz <geolangsat@gmail.com>
2016-06-09 22:45:06 +0530

    Added Registration Validations.

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var uploads = multer({dest: './uploads'});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -16,6 +18,70 @@ router.get('/login', function(req, res, next) {
   res.render('login',{
     'title': 'Login' //Will make Login highlight
   });
-}); 
+});
 
+router.post('/register', uploads.single('profileimage'), function(req,res,next){
+  var name = req.body.name;
+  var email = req.body.email;
+  var username = req.body.username;
+  var password = req.body.password;
+  var password2 = req.body.password2;
+
+
+  if(req.files && req.files.profileimage){
+    console.log('Uploading File...');
+
+    //File Info
+    var profileImageOriginalName = req.files.profileimage.originalname;
+    var profileImageName = req.files.profileimage.name;
+    var profileImageMime = req.files.profileimage.mimetype;
+    var profileImagePath = req.files.profileimage.path;
+    var profileImageExt = req.files.profileimage.extension;
+    var profileImageSize = req.files.profileimage.size;
+  }else{
+    var profileImageName = 'noImage.png';
+  }
+
+  //Form Validation
+  req.checkBody('name','Name field is Required').notEmpty();
+  req.checkBody('email','Email field is required').notEmpty();
+  req.checkBody('email','Email invalid').isEmail();
+  req.checkBody('username','Username field is required').notEmpty();
+  req.checkBody('password','Password field is required').notEmpty();
+  req.checkBody('password2','Passwords do not match').equals(req.body.password);
+
+  //Check for errors
+  var errors = req.validationErrors();
+
+  if(errors){
+    res.render('register',{
+      errors: errors,
+      name: name,
+      email: email,
+      username: username,
+      password: password,
+      password2: password2
+    });
+  }else{
+    var newUser = new User({
+      name: name,
+      email: email,
+      username: username,
+      password: password,
+      profileimage: profileImageName
+    });
+
+    //Create User
+    //User.createUser(newUser, function(err, user){
+    //  if(err)throw err;
+    //  console.log(user);
+    //});
+
+    //Success Message
+    req.flash('Success', 'You are now registered and may log in');
+
+    res.location('/');
+    res.redirect('/');
+  }
+});
 module.exports = router;
